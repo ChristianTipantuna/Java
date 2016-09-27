10299721b7189a39ed8171ecf7fabc76c87bd412
Christian Tipantuña <ChristianTipantuna@users.noreply.github.com>
2016-09-27 00:56:37 -0500

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,8 @@
+//Instrucciones básicas:  
+//						- Compilar: javac HolaMundo.java
+//						- Ejecutar: java HolaMundo
+class HolaMundo {
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+	}
+}
