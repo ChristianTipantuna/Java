1a9f41d8aea2ebc79f7bdbb6410df36c3259a118
Christian Tipantuña <ChristianTipantuna@users.noreply.github.com>
2016-10-30 19:21:25 -0500

    Delete HolaMundo.java

--- HolaMundo.java
+++
@@ -1,8 +0,0 @@
-//Instrucciones básicas:  
-//						- Compilar: javac HolaMundo.java
-//						- Ejecutar: java HolaMundo
-class HolaMundo {
-	public static void main(String[] args) {
-		System.out.println("Hola mundo");
-	}
-}
